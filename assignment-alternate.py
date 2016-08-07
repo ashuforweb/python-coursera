e4aa7b64c92c8b372da79695a8c368dfdd8c76b6
ashuforweb <ashuforweb@gmail.com>
2016-08-07 22:44:13 +0800

    push

--- assignment-alternate.py
+++ assignment-alternate.py
@@ -1,4 +1,3 @@
 #This is 2 line solution for assignment1
 import re
-print sum( [ int(i) for i in re.findall('[0-9]+',open('regex_sum_42.txt').read()) ] )
-# print re.findall('[0-9]+',open('regex_sum_42.txt').read())
+print sum( [ int(i) for i in re.findall('[0-9]+',open('regex_sum_307201.txt').read()) ] )
